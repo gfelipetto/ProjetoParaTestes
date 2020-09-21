21ac4471ebdaeca3029b96883bb0afd1f554e21e
gfelipetto <gfelipetto@users.noreply.github.com>
2020-09-21 18:57:58 -0300

    Trab

---
+++ Assets/Scripts/MovimentoLobinho.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoLobinho : MonoBehaviour
+{
+    public Animator lobo;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            lobo.SetBool("Andar", true);
+        }
+    }
+}

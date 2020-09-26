0d0d7a8a1155abf504fdd3695d35539ebe5e2f1d
gfelipetto <gfelipetto@users.noreply.github.com>
2020-09-26 18:43:06 -0300

    Lobinho pulo e ataque

--- Assets/Scripts/MovimentoLobinho.cs
+++ Assets/Scripts/MovimentoLobinho.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -15,7 +16,7 @@ public class MovimentoLobinho : MonoBehaviour
     public float speed = 6.0f;
     public float suavizar = 0.1f;
     public float groundDistance = 0.4f;
-    public float pularAltura = 10f;
+    public float pularAltura = 4f;
     public float gravidade = -9.81f;
     float suavizarVelocidade;
 
@@ -24,11 +25,15 @@ public class MovimentoLobinho : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
-    private void Update()
+    private void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direcao = new Vector3(horizontal, 0f, vertical).normalized;
+        if (!NoChao() && pular.y < 0)
+        {
+            pular.y = 1.5f;
+        }
         if (direcao.magnitude >= 0.1f)
         {
             float pontoAngulo = Mathf.Atan2(direcao.x, direcao.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -37,24 +42,41 @@ public class MovimentoLobinho : MonoBehaviour
             Vector3 mover = Quaternion.Euler(0f, pontoAngulo, 0f) * Vector3.forward;
             controlePersonagem.Move(mover.normalized * speed * Time.deltaTime);
         }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 10f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 4f;
+        }
         if (NoChao())
         {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                speed = 10f;
-            }
-            if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                speed = 6f;
-            }
             if (Input.GetButtonDown("Jump"))
             {
-                pular.y = Mathf.Sqrt(pularAltura * -3f * gravidade);
+                pular.y = Mathf.Sqrt(pularAltura * -2f * gravidade);
             }
-            animacao.SetFloat("Velocidade", controlePersonagem.velocity.magnitude);
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                animacao.SetTrigger("Atacar");
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+
+            }
         }
+        animacao.SetFloat("Velocidade", controlePersonagem.velocity.magnitude);
         pular.y += gravidade * Time.deltaTime;
         controlePersonagem.Move(pular * Time.deltaTime);
+        if (NoChao())
+        {
+            animacao.SetBool("Pular", false) ;
+        }
+        if (!NoChao())
+        {
+            animacao.SetBool("Pular", true);
+        }
     }
 
     private bool NoChao()

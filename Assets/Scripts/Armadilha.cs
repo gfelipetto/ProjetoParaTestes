f8f18f7a293f2cddb81d32d7ab71aa736823f24e
cesarcadona <52334792+cesarcadona@users.noreply.github.com>
2020-09-27 18:42:26 -0300

    trap

--- Assets/Scripts/Armadilha.cs
+++ Assets/Scripts/Armadilha.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class Armadilha : MonoBehaviour
 {
+    private MovimentoLobinho lobo;
+
+    private void Start ( ) {
+        lobo = FindObjectOfType<MovimentoLobinho>();
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Lobisomem")
         {
+            
+            lobo.enabled = false;
             this.gameObject.GetComponent<Animator>().SetTrigger("AtivarArmadilha");
             Destruir();
         }
     }
     private void Destruir()
     {
+        
         Destroy(this.gameObject, 3f);
+    }
+
+    private void OnDestroy ( ) {
+        
+        lobo.enabled = true;
     }
 }

0dd88699258cd861ba7fc8e21046261dbb99a6df
gfelipetto <gfelipetto@users.noreply.github.com>
2020-09-27 18:11:49 -0300

    Armadilhas

---
+++ Assets/Scripts/Armadilha.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armadilha : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Lobisomem")
+        {
+            this.gameObject.GetComponent<Animator>().SetTrigger("AtivarArmadilha");
+            Destruir();
+        }
+    }
+    private void Destruir()
+    {
+        Destroy(this.gameObject, 3f);
+    }
+}

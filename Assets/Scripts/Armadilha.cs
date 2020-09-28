34617d3d9310eef11d02f9e28af6a4039f4e074c
gfelipetto <gfelipetto@users.noreply.github.com>
2020-09-28 12:20:39 -0300

    Teste

--- Assets/Scripts/Armadilha.cs
+++ Assets/Scripts/Armadilha.cs
@@ -5,29 +5,18 @@ using UnityEngine;
 public class Armadilha : MonoBehaviour
 {
     private MovimentoLobinho lobo;
-
-    private void Start ( ) {
-        lobo = FindObjectOfType<MovimentoLobinho>();
-    }
-    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Lobisomem")
         {
-            
-            lobo.enabled = false;
             this.gameObject.GetComponent<Animator>().SetTrigger("AtivarArmadilha");
+            lobo = other.GetComponent<MovimentoLobinho>();
+            lobo.animacao.SetBool("Uivar", true);
             Destruir();
         }
     }
     private void Destruir()
     {
-        
         Destroy(this.gameObject, 3f);
-    }
-
-    private void OnDestroy ( ) {
-        
-        lobo.enabled = true;
     }
 }
